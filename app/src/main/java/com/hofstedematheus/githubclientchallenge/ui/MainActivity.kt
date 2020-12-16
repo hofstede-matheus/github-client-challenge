3fd5c640a2b3618ebc06385724e29942339de9b8
hofstede-matheus <matheus.hofstede@cubos.io>
2020-12-16 08:38:32 -0300

    (feat/splash_screen) add lightweight splash screen

--- app/src/main/java/com/hofstedematheus/githubclientchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/hofstedematheus/githubclientchallenge/ui/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_GithubClientChallenge)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
